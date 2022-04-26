a28895fb783e3d4cd964feb425776a2597ec2f1e
Andres <andresr774@gmail.com>
2022-04-26 17:00:05 -0500

    Initial Commit

---
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootcampApp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Andres camilo Raigoza misas on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
